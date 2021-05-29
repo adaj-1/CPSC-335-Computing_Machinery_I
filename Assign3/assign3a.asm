354d31c4d2cf5ab12e4906c93614324c3026002c
adaj-1 <67992220+adaj-1@users.noreply.github.com>
2021-05-29 11:12:06 -0600

    updated again

--- Assign3/assign3a.asm
+++ Assign3/assign3a.asm
@@ -4,26 +4,21 @@
 // Jada  Li
 // UCID: 30016807
 
-// 1111 0001 0010 = F12 do you want the register to contain -12 or binary in integers 1111 0011 
-
-// just to confirm the BCD will be entered with no spaces correct?
-
 .data						// must be delcared to call scanf()
 n: 	.word 0					// must be delcared to call scanf()
 
 .text
 scan:		.string "%ld"						// holds user input in long integer
-prompt:		.string "Please enter N (a number in binary):\n"		// prompts user to input 
-printTrueValue:	.string "%d\n"
-printNeg:	.string "-"
+prompt:		.string "Please enter N (a number in binary):\n"	// prompts user to input binary 
+printBCDValue:	.string "%d\n"						// used to print true value
+printNeg:	.string "-"						// user to print negative
 
 		.balign 4			// ensures instructions are properly aligned
 		.global main			// makes the label "main" visible to the linker
 
-		define(userInput, x19)
-		define(BCDValue, x20)
-		define(trueValue, x21)		
-		define(binaryValue, x26)
+		define(userInput, x19)		// macro to hold user input
+		define(binaryValue, x20)	// macro to hold binaryValue
+		define(BCDValue, x21)		// macro to hold BCD value
 
 main:		stp x29, x30, [sp, -16]!	// saves the state of the registers used by calling code
 		mov x29, sp			// updates FP to the current SP
@@ -39,53 +34,55 @@ userInput:	ldr x0, =prompt			// prompt message
 		ldr x14, =n			// load address =n to register x14
 		ldr userInput, [x14]		// load the value of n 
 
+// setting up to convert binary to BCD value
 calculate:	mov x10, 10			// initializing constant for finding remainder
 		mov x11, 0			// intializing exponential counter
 		mov x13, 1			// initializing value for conversion to decimal
-		mov x23, 0			// initializing register to hold BCDValue
-		mov trueValue, 0
+		mov x23, 0			// initializing register to hold binaryValue
+		mov BCDValue, 0			// initializing BCDValue
 		mov x24, 0			// initializing positive value
-			
+
+// loop to calculate BCD value			
 bcdLoop:	udiv x9, userInput, x10		// calculating quotient
 		msub x22, x9, x10, userInput	// calculating modulus
 		
-		mov userInput, x9		// udpating userInput after divided by 10
+		mov userInput, x9		// updating userInput after divided by 10
 	
 		lsl x12, x22, x11		// LSL to find to the power two
-		add BCDValue,BCDValue, x12	// calculating decimal value
+		add binaryValue,binaryValue, x12	// calculating decimal value
 		
 		add x11, x11, 1			// increments exponential counter
 		cmp x11, 4			// if less than 4 it is the end of the BCD section
-		b.lt bcdLoop
+		b.lt bcdLoop			// loops back to bcd if its not done calculating binary
 		
 checkNeg:	cmp userInput, 0		// only run code if this is for most significant digits
-		b.ne findingTrueValue
+		b.ne findingBCDValue		// skipping over if user inptu was not negative
 		mov x15, 15			// to hold negative value
-		cmp x15, BCDValue
-		b.ne findingTrueValue		// to account for negative value	
+		cmp x15, binaryValue		// cheking to see if input value was negative
+		b.ne findingBCDValue		// to account for negative value	
 	
-		ldr x0, =printNeg	
-		bl printf
+		ldr x0, =printNeg		// printing negative symbol
+		bl printf			// printing negative symbol
 
-		b decimalToBinary
+		b binaryToBCD		// branching to print
 	
-findingTrueValue:
-		mul x23, BCDValue, x13		// calculating correct decimal place
-		add trueValue, trueValue, x23	// calculating true value
+findingBCDValue:
+		mul x23, binaryValue, x13	// calculating correct decimal place
+		add BCDValue, BCDValue, x23	// calculating true value
 		
 		mul x13, x13, x10		// incrementing to next decimal place
 		mov x11, 0			// to count next set of bits	
-		mov x14, 0
-		mov x14, BCDValue	
-		mov BCDValue, 0		
+		mov x14, 0			// initialized to hold binaryValue	
+		mov x14, binaryValue		// passing in binaryValue
+		mov binaryValue, 0		// zeroing binaryValue to calculate next binary bits
 		cmp userInput, xzr		// check if userInput is 0 now
 		b.ne bcdLoop			
 	
 
-decimalToBinary:
-		ldr x0, =printTrueValue
-		mov x1, trueValue
-		bl printf	
+binaryToBCD:
+		ldr x0, =printBCDValue		// printing BCD Value
+		mov x1, BCDValue		// printing BCD Value
+		bl printf			// printing BCD Value
 
 exit:		ldp 	x29,	x30, 	[sp], 	16 	// restores state
 		ret					// returns control to calling code
--- Assign3/assign3b.asm
+++ Assign3/assign3b.asm
@@ -1,13 +1,9 @@
 // assign3b.asm
 // CPSC 355 Assignment 3
-// binary to BCD 
+// BCD to Binary 
 // Jada Li
 // UCID: 3001680
 
-
-// how to indicate negative binary input. will you enter -1010 0110, 1111  010101, or will we have to do something else like bit extend will it also include spaces or no spaces
-
-// for the program output do you expect printing in decimal using bcd code or in binary format like 1111 0011 with space or 11110011 no spaces
 .data						// must be delcared to call scanf()
 n: 	.word 0					// must be delcared to call scanf()
 
@@ -20,9 +16,9 @@ nextLine:	.string "\n"						// used to after printing binary
 		.balign 4			// ensures instructions are properly aligned
 		.global main			// makes the label "main" visible to the linker
 
-		define(userInput, x19)
-		define(BCDLength, x23) 		
-		define(BCDValue, x24) 
+		define(userInput, x19)		// macro to hold user input
+		define(binaryLength, x23) 	// macro to count how long binary number is
+		define(binaryValue, x24) 	// macro to hold binary value
 		
 
 main:		stp x29, x30, [sp, -16]!	// saves the state of the registers used by calling code
@@ -41,16 +37,18 @@ userInput:	ldr x0, =prompt			// prompt message
 		mov x10, 10			// initializing contant for finding remainder
 		mov x11, 0			// temp register for exponential counter
 
+// setting up conversion to binary
 convertBCD:	mov x12, 0			// initializing number of bits to shift
 		mov x27, 0			// initializing positive number 
-		mov BCDLength, 0		// initializing BCDLength
+		mov binaryLength, 0		// initializing binaryLength
 
 		cmp userInput, 0		// check if negative number
 		b.gt BCDLoop			// branch to BCDLoop
 		mov x27, 1			// setting as flag for negative
 		neg userInput, userInput	// removing negative from trueValue to properly convert to BCD
-		
-BCDLoop:	add BCDLength, BCDLength, 4	// to track length of bits
+
+// looping to converting userInput to binary
+BCDLoop:	add binaryLength, binaryLength, 4	// to track length of bits
 		mov x9, 0			// initializing quotient
 		mov x10, 10			// initializing denominator
 		
@@ -59,42 +57,45 @@ BCDLoop:	add BCDLength, BCDLength, 4	// to track length of bits
 
 		mov userInput, x9		// copying quotient to trueValue
 	
-		lsl x13, x22, x12 		// LSL to shift to the proper BCD position
+		lsl x13, x22, x12 		// LSL to shift to the proper binary position
 		add x12, x12, 4			// adding 4 to shift to next digit
 		
-		add BCDValue, BCDValue, x13	// storing back values in BCD Representation
+		add binaryValue, binaryValue, x13	// storing back values in binary
 
 		cmp userInput, xzr		// checking if finished converting all values
 		b.ne BCDLoop 
 	
-		mov x26, BCDLength
+		mov x26, binaryLength		// copying binaryLength to be used for printing
 
-checkNeg:	cmp x27, 1
-		b.ne printingBinaryAsBCD	// not negative
-		mov x28, 1111		
+// checks for negative input and prints accordingly
+checkNeg:	cmp x27, 1			// checking if user input was negative
+		b.ne printingBCDAsBinary	// not negative
+		mov x28, 1111			// adding leading 1's to indicate negative
 		
-		ldr x0, =decimalDigit		
-		mov x1, x28
-		bl printf			
-		
-printingBinaryAsBCD:
-		mov x25, BCDValue		// make copy of value
+		ldr x0, =decimalDigit		// printing leading 1's
+		mov x1, x28			// printing leading 1's
+		bl printf			// printing leading 1's
+
+// prints BCD value as Binary
+printingBCDAsBinary:
+		mov x25, binaryValue		// make copy of value
 		sub x26, x26, 1			// number of bits to shift
 		ror x25, x25, x26		// rotate right by number of bits
 		and x25, x25, 0x1		// mask by everything but last digit
-		cmp x26, xzr
-		b.lt exit	
+		cmp x26, xzr			// check if we have finished printing binary
+		b.lt exit			// exit when done
 			
-		ldr x0, =decimalDigit		
-		mov x1, x25
-		bl printf			
+		ldr x0, =decimalDigit		// printing each binary digit
+		mov x1, x25			// printing each binary digit
+		bl printf			// printing each binary digit
+		
+		b printingBCDAsBinary		// looping until printing is complete
 
-		b printingBinaryAsBCD
-
-exit:		ldr x0, =nextLine		
-		bl printf			
+exit:		ldr x0, =nextLine		// printing next line for formating
+		bl printf			// printing next line for formatting
 		
 		ldp 	x29,	x30, 	[sp], 	16 	// restores state
 		ret					// returns control to calling code
+
 
 
